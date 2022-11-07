a5802e70e1bd51d629a21161ae897795c0804c93
HRISHIKESH <111124015+hrishi141220@users.noreply.github.com>
2022-11-07 15:59:43 +0530

    Add files via upload

---
+++ duct.js
@@ -0,0 +1,10 @@
+const holdCabinet1=['duct tape', 'gum', 3.14, false, 6.02223] ;
+const holdCabinet2=['orange drink', 'nerf toys', 'camera', 42, 'parsnip' ];
+console.log(holdCabinet1.concat(holdCabinet2));
+console.log(holdCabinet1);
+console.log(holdCabinet1.slice(2));
+ holdCabinet2.slice (2);
+holdCabinet1.reverse();
+holdCabinet2.sort();
+console.log(holdCabinet1);
+console.log(holdCabinet2);
